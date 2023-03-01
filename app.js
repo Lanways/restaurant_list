eec99e801b656fdf3d3516a6f4ced0f1246220d3
Lanways <mcbluedd@gmail.com>
2023-03-01 15:38:18 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const Restaurant = require('./models/Restaurant')
-
+// 載入bodyparser
+const bodyParser = require('body-parser')
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -26,7 +27,8 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 //render partial template 瀏覽全部餐廳
@@ -36,6 +38,18 @@ app.get('/', (req, res) => {
     .then(restaurantData => res.render('index', { restaurantData }))
     .catch(err => console.log(error))
 })
+// 新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+// 新增餐廳
+app.post('/restaurants', (req, res) => {
+  console.log(req.body)
+  Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(error))
+})
+
 //show page
 app.get('/restaurants/:restaurant_id', (req, res) => {
   console.log('restaurant_id', req.params.restaurant_id)
