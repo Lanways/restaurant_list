06e7464ba406cef27b077a54b92990be9d029b04
Lanways <mcbluedd@gmail.com>
2023-03-03 14:20:09 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
 const express = require('express')
-// 載入 mongoose
-const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
-const Restaurant = require('./models/Restaurant')
-// 載入bodyparser
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-// 僅在非正式環境時, 使用 dotenv
+require('./config/mongoose')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
-}
-
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb connected')
-})
+} // 僅在非正式環境時, 使用 dotenv
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true })) //每一筆請求都需要透過 body-parser 進行前置處理
 app.use(methodOverride('_method'))
 app.use(routes)
 
-
-
-
-//監聽並啟動伺服器
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
-})
+}) //監聽並啟動伺服器
---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+module.exports = db
