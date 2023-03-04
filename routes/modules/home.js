fa050869cc4514715874ee0bf623dccef1e44fdc
Lanways <mcbluedd@gmail.com>
2023-03-04 18:08:06 +0800

    feat: modify READEME IMAGE

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/Restaurant')
-//render partial template 瀏覽全部餐廳
+// render partial template 瀏覽全部餐廳
 router.get('/', (req, res) => {
   Restaurant.find() // 取出 Restaurant model 裡的所有資料
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
