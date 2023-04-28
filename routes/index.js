ed1e9fe2c6ed04427b6289ac73d45a4ca093fc89
Lanways <mcbluedd@gmail.com>
2023-04-28 14:44:47 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
-router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
+router.use('/', home)
+
 module.exports = router
