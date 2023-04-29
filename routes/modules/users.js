15af137302ef70ea30b25c7a705926733f3c15bf
Lanways <mcbluedd@gmail.com>
2023-04-29 15:30:50 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -41,4 +41,9 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/logout', (req, res) => {
+  req.logout() //Passport.js 提供的函式，會幫你清除 session
+  res.redirect('/users/login')
+})
+
 module.exports = router
