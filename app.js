6fd427caecb194743a7300edd245fd43cb368e24
Lanways <mcbluedd@gmail.com>
2023-04-30 17:59:26 +0800

    feat: add facebook login

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const session = require('express-session')
@@ -10,7 +9,7 @@ const usePassport = require('./config/passport')
 const routes = require('./routes')
 
 require('./config/mongoose')
-
+const port = process.env.PORT
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 } // 僅在非正式環境時, 使用 dotenv
@@ -19,7 +18,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(session({
-  secret: 'ThisMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: 'false',
   saveUninitialized: true
 }))
@@ -29,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true })) //每一筆請求都需要透
 app.use(methodOverride('_method'))
 
 usePassport(app)
-app.use(flash()) 
+app.use(flash())
 //locals是express幫我們開的一條捷徑，裡面的資料所有view都可以存取
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const FacebookStrategy = require('passport-facebook').Strategy
+
 
 module.exports = app => {
   app.use(passport.initialize())
@@ -27,6 +29,34 @@ module.exports = app => {
         })
         .catch(err => done(err, false))
     }))
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) return done(null, user)
+
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
+    }
+  ))
+
   // serialize/deserialize 序列化/反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
